6e95dcc6d3c19fb8cd43913991fc6cc105e4ab0b
ivar <ivar.lovlie@knowit.no>
2025-09-19 12:02:41 +0200

    .

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Select } from "@postenbring/hedwig-react";
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<Select label="Test" size="small">
+				<option value="1">
+					123123123123123123123123123123123123123123123123123123
+				</option>
+				<option value="1">123</option>
+			</Select>
+		</div>
+	);
+}
+
+export default App;
